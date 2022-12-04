9af88b5a70b0b1a5ac5ab80eb23a5aa308ab3342
Ashique <ashique12009@gmail.com>
2022-12-04 20:39:11 +0600

    components added

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="container mt-5">
+        <div class="col-12 text-center">
+            <h1>Welcome to my blogs</h1>
+            <a href="https://ashique12009.blogspot.com" target="_blank">Visit For More Blogs</a>
+        </div>
+    </div>
+</template>
