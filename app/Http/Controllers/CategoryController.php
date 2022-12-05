31ad42ad55f3cf28e35baf9145054b1c1aa7ff4f
Ashique <ashique12009@gmail.com>
2022-12-05 13:17:39 +0600

    delete operation done

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -68,6 +68,9 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response()->json([
+            'message'=>'Category Deleted Successfully!'
+        ]);
     }
 }
